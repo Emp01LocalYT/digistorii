0253224879fdcc935bf058e2e98eadef91d23867
Emp01LocalYT <yaanartechyaanartech@gmail.com>
2026-06-25 10:56:16 +0530

    public tables in db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,24 @@
 import { Pool,types } from "pg";
+import { ensureDB } from "./ensure-db";
+
+
+
+ 
+
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
+export async function query(text: string, params?: any[]) {
+  await ensureDB(); 
+  
+  const start = Date.now();
+  const res = await pool.query(text, params);
+  const duration = Date.now() - start;
 
-types.setTypeParser(1082, (val: string) => val);
+  console.log("executed query", { text, duration, rows: res.rowCount });
+  return res;
+}
  
+types.setTypeParser(1082, (val: string) => val);
